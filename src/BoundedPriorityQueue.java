fc7eae070eb933497e99a95083f3ebcf16b5a224
root <jusblazian@yahoo.ie>
2024-04-30 22:49:07 +0100

    updated addLast method from LinkedList in BoundedPriorityQueue

--- src/BoundedPriorityQueue.java
+++ src/BoundedPriorityQueue.java
@@ -10,4 +10,22 @@ public class BoundedPriorityQueue extends LinkedList {
         this.capacity = capacity;
         this.doctorName = doctorName.toLowerCase();
     }
+
+    /**
+     *adds an appointment to the end of the queue if the appointment's doctor matches the queue's specified doctor
+     */
+    @Override
+    public void addLast(Appointment appointment) {
+        if (!appointment.getDoctorFullName().equalsIgnoreCase(doctorName)) {
+            System.out.println("Cannot add appointment: Doctor names do not match.");
+            return;
+        }
+
+        if (size() >= capacity) {
+            System.out.println("Cannot add appointment: Queue is full.");
+            return;
+        }
+
+        super.addLast(appointment);
+    }
 }
