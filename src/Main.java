611a8205ffa12b8f9a3e5d6718cd7eeeacea4462
JoArtMahilaga <Joartmahilaga69@gmail.com>
2024-04-30 13:41:41 +0100

    patient done overrides added

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,32 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        public class Appointment implements Comparable<Appointment> {
+            private String firstName;
+            private String lastName;
+            private LocalDate dob;
+            private String issue;
+            private LocalDate date;
+            private int triageLevel;
+            private String doctorFullName;
+
+            @Override
+            public boolean equals(Object obj) {
+                if (this == obj) return true;
+                if (obj == null || getClass() != obj.getClass()) return false;
+                Appointment that = (Appointment) obj;
+                return triageLevel == that.triageLevel &&
+                        firstName.equals(that.firstName) &&
+                        lastName.equals(that.lastName) &&
+                        dob.equals(that.dob) &&
+                        issue.equals(that.issue) &&
+                        date.equals(that.date) &&
+                        doctorFullName.equals(that.doctorFullName);
+            }
+
+
+        }
     }
 }
